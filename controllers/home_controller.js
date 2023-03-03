fe4840f88286b9603391e308317fdf9d46743a9b
Ashis <developer.ashis99@gmail.com>
2023-03-03 13:10:43 +0530

    added a controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function(req,res){
+    return res.end ("<h1> Express is up for TODO List</h1>")
+}
+
+console.log("Home Controller Loaded");
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 6000; //by default browser runs on port 80;
+const port = 8000; //by default browser runs on port 80;
+
+
+// Use Router
+app.use('/', require("./routes"));
+
+
 
 app.listen(port, function(err){
     if(err){
